2ecd7009fe86f34be756ca71fd821adbbb060b39
zhangqi00 <562895852@qq.com>
2020-06-16 18:27:33 +0800

    'babel原理'

--- 19.webpack相关.js
+++ 19.webpack相关.js
@@ -50,4 +50,65 @@
 
 // 看到这里你会发现，代码里有一处会判断返回值是否是Promise（typeof result.then === "function"），如果是Promise则会异步调用callback。因此，想要获得一个异步的loader，除了webpack文档里提到的this.async()方法，还可以直接返回一个Promise。
 
-//具体可看这个： https://juejin.im/post/5bc1a73df265da0a8d36b74f
+//具体可看这个： https://juejin.im/post/5bc1a73df265da0a8d36b74f  很有深度
+
+/**
+ * plugin
+ *
+ * webpack 在编译过代码程中，会触发一系列 Tapable 钩子事件，插件所做的，就是找到相应的钩子，往上面挂上自己的任务，也就是注册事件，这样，当 webpack 构建的时候，插件注册的事件就会随着钩子的触发而执行了。
+ */
+
+/**
+ * webpack 打包的大致过程
+  * 将命令行参数与 webpack 配置文件 合并、解析得到参数对象。
+参数对象传给 webpack 执行得到 Compiler 对象。
+执行 Compiler 的 run方法开始编译。每次执行 run 编译都会生成一个 Compilation 对象。
+触发 Compiler 的 make方法分析入口文件，调用 compilation 的 buildModule 方法创建主模块对象。
+生成入口文件 AST(抽象语法树)，通过 AST 分析和递归加载依赖模块。
+所有模块分析完成后，执行 compilation 的 seal 方法对每个 chunk 进行整理、优化、封装。
+最后执行 Compiler 的 emitAssets 方法把生成的文件输出到 output 的目录中。
+  */
+
+// 下面是一个webpack 插件demo，生成文件清单
+class FileListPlugin {
+  constructor(options) {
+    // 获取插件配置项
+    this.filename =
+      options && options.filename ? options.filename : "FILELIST.md";
+  }
+
+  apply(compiler) {
+    // 注册 compiler 上的 emit 钩子
+    compiler.hooks.emit.tapAsync("FileListPlugin", (compilation, cb) => {
+      // 通过 compilation.assets 获取文件数量
+      let len = Object.keys(compilation.assets).length;
+
+      // 添加统计信息
+      let content = `# ${len} file${len > 1 ? "s" : ""} emitted by webpack\n\n`;
+
+      // 通过 compilation.assets 获取文件名列表
+      for (let filename in compilation.assets) {
+        content += `- ${filename}\n`;
+      }
+
+      // 往 compilation.assets 中添加清单文件
+      compilation.assets[this.filename] = {
+        // 写入新文件的内容
+        source: function() {
+          return content;
+        },
+        // 新文件大小（给 webapck 输出展示用）
+        size: function() {
+          return content.length;
+        }
+      };
+
+      // 执行回调，让 webpack 继续执行
+      cb();
+    });
+  }
+}
+
+module.exports = FileListPlugin;
+
+// 热更新原理  https://mp.weixin.qq.com/s/2L9Y0pdwTTmd8U2kXHFlPA 这个不错
---
+++ 20.babel.js
@@ -0,0 +1,158 @@
+/**
+ * babel
+ * 简单来说，babel是能够编译成es2015+代码，兼容浏览器运行
+ */
+
+//  babel 是如何工作的   抽象语法树（Abstract Syntax Tree, AST），Babel 本质上就是在操作 AST 来完成代码的转译。
+// AST：  https://ASTexplorer.net
+
+/**
+ * 大多数编译器的工作：
+ *  1. Parse(解析)：将源代码转换成更加抽象的表示方法(抽象语法树)
+ *  2. Transform(转化): 对抽象语法树做一些特殊处理，让它更符合编译器的期望
+ *  3. Generate(代码生成)：将第二步经过转换的抽象语法树生成新的代码
+ */
+
+// Parse 解析：一般来说，Parse可细分为两个阶段：词法分析，语法分析
+/**
+ * 词法分析：
+ * 词法分析阶段可以看成是对代码进行“分词”，它接收一段源代码，然后执行一段 tokenize 函数，把代码分割成被称为Tokens 的东西。Tokens 是一个数组，由一些代码的碎片组成，比如数字、标点符号、运算符号等等等等，例如下面这样：
+ */
+// const add = (a, b) => a + b;
+//Tokens: [
+//   ({ type: "Keyword", value: "const" },
+//   { type: "Identifier", value: "add" },
+//   { type: "Punctuator", value: "=" },
+//   { type: "Punctuator", value: "(" },
+//   { type: "Identifier", value: "a" },
+//   { type: "Punctuator", value: "," },
+//   { type: "Identifier", value: "b" },
+//   { type: "Punctuator", value: ")" },
+//   { type: "Punctuator", value: "=>" },
+//   { type: "Identifier", value: "a" },
+//   { type: "Punctuator", value: "+" },
+//   { type: "Identifier", value: "b" })
+// ];
+/**
+ * 语法分析：
+ * 「词法分析」之后，代码就已经变成了一个 Tokens 数组了，现在需要通过「语法分析」把 Tokens 转化为上面提到过的 AST。
+ *
+ * 思考：如何实现一个parse把上面的Tokens转换成语法树(如下面const add = (a, b) => a + b;的一段语法树)
+ * 官方实现：https://github.com/babel/babel/tree/master/packages/babel-parser/src/parser
+ * 什么是二项式？？？
+ */
+// {
+//     "type": "Program",
+//         "body": [
+//             {
+//                 "type": "VariableDeclaration", // 变量声明
+//                 "declarations": [ // 具体声明
+//                     {
+//                         "type": "VariableDeclarator", // 变量声明
+//                         "id": {
+//                             "type": "Identifier", // 标识符（最基础的）
+//                             "name": "add" // 函数名
+//                         },
+//                         "init": {
+//                             "type": "ArrowFunctionExpression", // 箭头函数
+//                             "id": null,
+//                             "expression": true,
+//                             "generator": false,
+//                             "params": [ // 参数
+//                                 {
+//                                     "type": "Identifier",
+//                                     "name": "a"
+//                                 },
+//                                 {
+//                                     "type": "Identifier",
+//                                     "name": "b"
+//                                 }
+//                             ],
+//                             "body": { // 函数体
+//                                 "type": "BinaryExpression", // 二项式
+//                                 "left": { // 二项式左边
+//                                     "type": "Identifier",
+//                                     "name": "a"
+//                                 },
+//                                 "operator": "+", // 二项式运算符
+//                                 "right": { // 二项式右边
+//                                     "type": "Identifier",
+//                                     "name": "b"
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 ],
+//                 "kind": "const"
+//             }
+//         ],
+//             "sourceType": "module"
+// }
+
+/**
+ *Transform(转换)
+  主要就是操作AST，Babel对于AST是深度优先遍历，
+  1. 声明了一个变量，并且知道了它的内部属性（id、init），然后我们再以此访问每一个属性以及它们的子节点。
+  2. id 是一个 Idenrifier，有一个 name 属性表示变量名。
+  3. 之后是 init，init 也有好几个内部属性：
+        type 是ArrowFunctionExpression，表示这是一个箭头函数表达式
+        params 是这个箭头函数的入参，其中每一个参数都是一个 Identifier 类型的节点；
+        body 属性是这个箭头函数的主体，这是一个 BinaryExpression 二项式：left、operator、right，分别表示二项式的左边变量、运算符以及右边变量。
+
+
+  Babel 会维护一个称作 Visitor 的对象，这个对象定义了用于 AST 中获取具体节点的方法。
+  babel遍历AST会经过两次节点，遍历的时候和退出的时候，所以实际上visitor看起来像这样的(实际上会复杂很多)
+  var visitor = {
+    Identifier: {
+        enter() {
+            console.log('Identifier enter');
+        },
+        exit() {
+            console.log('Identifier exit');
+        }
+    }
+  };
+
+  箭头函数是 ES5 不支持的语法，所以 Babel 得把它转换成普通函数，一层层遍历下去，找到了 ArrowFunctionExpression 节点，这时候就需要把它替换成 FunctionDeclaration 节点。所以，箭头函数可能是这样处理的
+
+  源码：https://github.com/babel/babel/tree/master/packages/babel-traverse。
+ */
+
+/**
+ * Generate(代码生成)
+ * 经过上面两个阶段，需要转译的代码已经经过转换，生成新的 AST 了，最后一个阶段理所应当就是根据这个 AST 来输出代码。
+
+    Babel 是通过 https://github.com/babel/babel/tree/master/packages/babel-generator 来完成的。当然，也是深度优先遍历
+ */
+
+/**
+ * babel 处理流程：Tokenizer(词法分析，将源码分割成Tokens数组)->Parser(语法解析,将Tokens数组转化成AST)->Traverser(遍历AST并应用转换器)-> Transformer(AST转换器，增删改查AST节点)->Generator(代码生成，将AST转换成源代码)
+ */
+
+/**
+ * babel 核心
+ * 1.@babel/core
+ *   + 加载和处理配置
+ *   + 加载插件
+ *   + 调用parse进行语法解析，生成Ast
+ *   + 调用Traverser遍历AST，并使用 ***访问者模式***(???) 应用插件对Ast进行转换
+ *   + 生成代码，包括SourceMap转换和源码生成
+ * 2. parser(@babel/parser)解析，Traverser(@babel/traverse)转换 Generator(@babel/generator)生成代码
+ * 3.插件
+ *   语法插件(@babel/plugin-syntax-*) parser不支持扩展，所以这个实际上就是开启parser的某个功能特性
+ *   转换插件 @babel/plugin-transform-* 普通的转换插件，
+ *   预定义集合(@babel/presets-*) 插件集合或者分组，主要方便用户对插件进行管理和使用。比如preset-env含括所有的标准的最新特性; 再比如preset-react含括所有react相关的插件.
+ * 4.插件开发辅助
+    @babel/template： 某些场景直接操作AST太麻烦，就比如我们直接操作DOM一样，所以Babel实现了这么一个简单的模板引擎，可以将字符串代码转换为AST。比如在生成一些辅助代码(helper)时会用到这个库
+
+    @babel/types： AST 节点构造器和断言. 插件开发时使用很频繁
+
+    @babel/helper-*： 一些辅助器，用于辅助插件开发，例如简化AST操作
+
+    @babel/helper： 辅助代码，单纯的语法转换可能无法让代码运行起来，比如低版本浏览器无法识别class关键字，这时候需要添加辅助代码，对class进行模拟。
+ *
+ */
+
+/**
+ * 插件是顺序调用的，所以新的的插件（实验性的）放在前面，老的插件定义在后面，因为可能需要新的插件解析过老的插件才会识别
+ */
